f29d6e9bcaee0cac32e31d63cab776a3aeaf01ca
Adit-009 <147326770+Adit-009@users.noreply.github.com>
2026-03-03 12:36:13 +0530

    Mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,45 @@
 import { useState } from "react";
+
 const App = () => {
   const [color, setColor] = useState("#387478");
+
   return (
-    <div style={{ background: color }} className="h-screen w-full">
-      <h1 className="text-white text-center whitespace-nowrap text-6xl font-bold absolute top-80 left-1/2 transform -translate-x-1/2">
+    <div
+      style={{ background: color }}
+      className="min-h-screen w-full flex flex-col items-center justify-center px-4"
+    >
+      <h1 className="text-white text-center text-3xl sm:text-4xl md:text-6xl font-bold mb-12">
         Background Color Changer by Adit
       </h1>
-      <div className="flex flex-wrap fixed bottom-44 gap-8 w-full justify-center">
+
+      <div className="flex flex-wrap gap-4 sm:gap-6 justify-center">
         <button
           onClick={() => setColor("#B1F0F7")}
-          className="px-5 h-9 rounded-full font-semibold"
+          className="px-5 py-2 rounded-full font-semibold"
           style={{ background: "#B1F0F7" }}
         >
           LightBlue
         </button>
+
         <button
           onClick={() => setColor("#AB4459")}
-          className="px-5 h-9 rounded-full font-semibold text-white"
+          className="px-5 py-2 rounded-full font-semibold text-white"
           style={{ background: "#AB4459" }}
         >
           Brown
         </button>
+
         <button
           onClick={() => setColor("#85A98F")}
-          className="px-5 h-9 rounded-full font-semibold"
+          className="px-5 py-2 rounded-full font-semibold"
           style={{ background: "#85A98F" }}
         >
           LightGreen
         </button>
+
         <button
           onClick={() => setColor("#47663B")}
-          className="px-5 h-9 rounded-full font-semibold text-white"
+          className="px-5 py-2 rounded-full font-semibold text-white"
           style={{ background: "#47663B" }}
         >
           Green
@@ -39,4 +48,5 @@ const App = () => {
     </div>
   );
 };
+
 export default App;
