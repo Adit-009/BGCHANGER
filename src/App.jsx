c4a2e531e32a3e8a33437fe872c84ed1b4649517
Adit-009 <147326770+Adit-009@users.noreply.github.com>
2026-03-03 11:38:28 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+const App = () => {
+  const [color, setColor] = useState("#387478");
+  return (
+    <div style={{ background: color }} className="h-screen w-full">
+      <h1 className="text-white text-6xl font-bold absolute top-80 left-1/2 transform -translate-x-1/2">
+        Background Color Changer
+      </h1>
+      <div className="flex flex-wrap fixed bottom-44 gap-8 w-full justify-center">
+        <button
+          onClick={() => setColor("#B1F0F7")}
+          className="px-5 h-9 rounded-full font-semibold"
+          style={{ background: "#B1F0F7" }}
+        >
+          LightBlue
+        </button>
+        <button
+          onClick={() => setColor("#AB4459")}
+          className="px-5 h-9 rounded-full font-semibold text-white"
+          style={{ background: "#AB4459" }}
+        >
+          Brown
+        </button>
+        <button
+          onClick={() => setColor("#85A98F")}
+          className="px-5 h-9 rounded-full font-semibold"
+          style={{ background: "#85A98F" }}
+        >
+          LightGreen
+        </button>
+        <button
+          onClick={() => setColor("#47663B")}
+          className="px-5 h-9 rounded-full font-semibold text-white"
+          style={{ background: "#47663B" }}
+        >
+          Green
+        </button>
+      </div>
+    </div>
+  );
+};
+export default App;
